1b306a9de4383143f9a948739ae2470e668cf834
Phongsathorn Kittiworapanya <Phongsathron@outlook.com>
2019-02-26 19:26:45 +0700

    Add link to login

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,6 +1,9 @@
 <template>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <router-link class="navbar-brand" :to="'/'">Cinema</router-link>
+  <ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex">
+    <li><router-link class="nav-item nav-link" :to="'/login'">Sign in</router-link></li>
+  </ul>
 </nav>
 </template>
 
