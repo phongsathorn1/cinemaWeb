c909fd8e470a89e2f5866bd4ee4134a16304a61b
Phongsathorn Kittiworapanya <Phongsathron@outlook.com>
2019-02-27 10:37:13 +0700

    Add search featuer

---
+++ src/components/searchBox.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="search-bar">
+        <div class="search-dialog">
+            <form>
+                <div class="row">
+                    <div class="col-6">
+                        <select class="select-theater" v-model="theather" @change="theaterSearch">
+                            <option value="เลือกโรงภาพยนต์">เลือกโรงภาพยนต์</option>
+                            <option value="เซนทรัล ปิ่นเกล้า">เซนทรัล ปิ่นเกล้า</option>
+                            <option value="เซนทรัล เวิร์ล">เซนทรัล เวิร์ล</option>
+                        </select>
+                    </div>
+                    <div class="col-6">
+                        <div class="input-group mb-3">
+                            <input id="searchbox" type="text" class="search-box form-control" placeholder="ค้นหาภาพยนตร์" v-model="keyword" @keyup="search">
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => {
+        return {
+            keyword: '',
+            theather: 'เลือกโรงภาพยนต์'
+        }
+    },
+    methods:{
+        search(){
+            console.log('change')
+            this.$emit('search', this.keyword)
+        },
+        theaterSearch(){
+            this.$emit('theatersearch', this.theather)
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+@import '@/style/main.scss';
+
+.search-bar{
+    margin-bottom: 20px;
+}
+
+.search-box{
+    border: 1px solid #55b776;
+}
+
+.search-dialog{
+    background: $third-color;
+    width: 700px;
+    margin: auto;
+    padding: 20px 10px;
+    padding-bottom: 10px;
+    line-height: 30px;
+    border-radius: 3px;
+}
+
+.select-theater{
+    width: 100%;
+    height: 40px;
+}
+</style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -3,12 +3,17 @@
     <billboard></billboard>
     <div id="new-movie-section">
       <div class="container">
+        <search-box
+        @search="search($event)"
+        @theatersearch="theatherSearch($event)"
+        ></search-box>
         <h1>ภาพยนต์ใหม่ที่กำลังฉาย</h1>
         <div class="flex-wrapper new-movie-container">
           <div class="movie-card-item" v-for="movie in movies" :key="movie.id">
             <router-link :to="'/movie/'+movie.id">
               <movie-card 
                 :title="movie.name.th" 
+                :enTitle="movie.name.en"
                 :date="movie.avaliable_date" 
                 :image="movie.image_url"
               >
@@ -22,19 +27,60 @@
 </template>
 
 <script>
-import Billboard from "../components/billboard.vue";
-import MovieCard from "../components/MovieCard.vue";
-import movies from "../movies.json";
+import Billboard from "../components/billboard.vue"
+import MovieCard from "../components/MovieCard.vue"
+import movies from "../movies.json"
+import SearchBox from '../components/searchBox.vue'
+import moment from 'moment'
 
 export default {
   components: {
     Billboard,
     MovieCard,
+    SearchBox
   },
   data: () => {
     return {
-      movies: movies.movies
+      movies: movies.movies,
+      theater: null
     };
+  },
+  methods:{
+    search(event){
+      let keyword = event.toLowerCase()
+      this.movies = movies.movies
+      let thSearch = []
+      if(keyword == ''){
+        this.theatherSearch(this.theater)
+      }else{
+        this.theatherSearch(this.theater)
+        this.movies = this.movies.filter(x => {
+          return x.name.en.toLowerCase().indexOf(keyword) >= 0 || x.name.th.toLowerCase().indexOf(keyword) >= 0
+        })
+      }
+    },
+    theatherSearch(event){
+      this.theater = event
+      this.movies = movies.movies
+      if(this.theater == "เลือกโรงภาพยนต์" || this.theater == null){
+        this.movies = movies.movies
+      }else{
+        let moviesTemp = []
+        this.movies.forEach(movie => {
+          movie.avaliables.forEach(avaliable =>{
+            if(moment(avaliable.day, "DD-MM-YYYY").isSame(new Date(), "day")){
+              avaliable.locations.forEach(location => {
+                if(location.name == this.theater){
+                  moviesTemp.push(movie)
+                }
+              })
+            }
+          })
+        });
+
+        this.movies = moviesTemp
+      }
+    }
   }
 };
 </script>
@@ -43,7 +89,7 @@ export default {
 $secondary-bg-color: #2b2b2b;
 
 #new-movie-section {
-  padding: 50px 0px;
+  padding: 30px 0px;
   background-color: $secondary-bg-color;
 }
 
